1c7a1288262de08bdedf8514b92be911c0a6bdfa
gonzadel <gonzadela01@gmail.com>
2025-05-30 14:30:22 -0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "eth-token-sweeper",
+  "version": "1.0.0",
+  "main": "sweeper.js",
+  "scripts": {
+    "start": "node sweeper.js"
+  },
+  "dependencies": {
+    "web3": "^4.2.2"
+  }
+}
